0d7f2802404ba35ad3522a61d67199967af19b1d
nimrosh <nimrodka@mta.ac.il>
2023-11-10 01:43:44 +0200

    cookies play

--- popup.js
+++ popup.js
@@ -1,30 +1,45 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var userInput = document.getElementById('userInput');
-    var urlList = []; // Array to store URLs
+    // Get all input elements
+    var inputs = document.querySelectorAll('input');
 
-    userInput.addEventListener('keydown', function(event) {
-        // Check if the pressed key is "Enter"
-        if (event.key === 'Enter') {
-            // Get the user input value
-            var url = userInput.value;
+    // Add event listeners to each input
+    inputs.forEach(function(input, index) {
+        input.addEventListener('keydown', function(event) {
+            // Check if the pressed key is "Enter"
+            if (event.key === 'Enter') {
+                // Prevent the default behavior (form submission)
+                event.preventDefault();
 
-            // Do something with the URL (for example, log it)
-            console.log('Submitted URL:', url);
-
-            // Store the URL in the array
-            urlList.push(url);
-
-            // Clear the input field
-            userInput.value = '';
-
-            // Call a function to display or process the list of URLs
-            displayUrls();
-        }
+                // If it's not the last input, focus on the next one
+                if (index < inputs.length - 1) {
+                    inputs[index + 1].focus();
+                } else {
+                    // If it's the last input, create the cookie
+                    createCookie();
+                }
+            }
+        });
     });
 
-    // Function to display or process the list of URLs
-    function displayUrls() {
-        // For now, simply log the list of URLs
-        console.log('Stored URLs:', urlList);
+    function createCookie() {
+        // Implement your cookie creation logic here
+        // Retrieve values from input fields and construct the cookie string
+        var name = document.getElementById('nameInput').value;
+        var value = document.getElementById('valueInput').value;
+        // ... (similarly for other input fields)
+
+        // Construct the cookie string
+        var cookieString = name + '=' + value;
+
+        // Set the cookie
+        document.cookie = cookieString;
+
+        console.log('Cookie created successfully!',document.cookie);
+        document.getElementById('cookie').innerText = document.cookie ;
+        
+        
+        //setTimeout(location.reload(),10000);
+
     }
 });
+
